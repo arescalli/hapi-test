ce672dd176ff623b9bdb66f8582fe87960cc3179
Alessandro Rescalli <alessandro.rescalli@binarysystem.eu>
2016-02-13 16:30:14 +0100

    correct insert

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.route({
     method: 'GET',
     path:'/hello', 
     handler: function (request, reply) {
-        var query = client.query('INSERT INTO test (id, desc) VALUES (1, \'test2\')');
+        var query = client.query('INSERT INTO test (desc) VALUES (\'test2\')');
         return reply('hello world');
     }
 });
